1754915472661830cc4331b09cdc1c8e3513de5e
nkusikevin <nkusikvnhart@gmail.com>
2024-04-16 21:17:33 +0200

    add starter js

--- main.js
+++ main.js
@@ -1,24 +1,104 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import { Renderer, Camera, Transform } from "ogl";
 
-setupCounter(document.querySelector('#counter'))
+export default class App {
+	constructor() {
+		this.createRenderer();
+		this.createCamera();
+		this.createScene();
+
+		this.onResize();
+
+		this.update();
+
+		this.addEventListeners();
+	}
+
+	createRenderer() {
+		this.renderer = new Renderer();
+
+		this.gl = this.renderer.gl;
+		this.gl.clearColor(0.79607843137, 0.79215686274, 0.74117647058, 1);
+
+		document.body.appendChild(this.gl.canvas);
+	}
+
+	createCamera() {
+		this.camera = new Camera(this.gl);
+		this.camera.fov = 45;
+		this.camera.position.z = 20;
+	}
+
+	createScene() {
+		this.scene = new Transform();
+	}
+
+	/**
+	 * Events.
+	 */
+	onTouchDown(event) {}
+
+	onTouchMove(event) {}
+
+	onTouchUp(event) {}
+
+	onWheel(event) {}
+
+	/**
+	 * Resize.
+	 */
+	onResize() {
+		this.screen = {
+			height: window.innerHeight,
+			width: window.innerWidth,
+		};
+
+		this.renderer.setSize(this.screen.width, this.screen.height);
+
+		this.camera.perspective({
+			aspect: this.gl.canvas.width / this.gl.canvas.height,
+		});
+
+		const fov = this.camera.fov * (Math.PI / 180);
+		const height = 2 * Math.tan(fov / 2) * this.camera.position.z;
+		const width = height * this.camera.aspect;
+
+		this.viewport = {
+			height,
+			width,
+		};
+	}
+
+	/**
+	 * Update.
+	 */
+	update() {
+		this.renderer.render({
+			scene: this.scene,
+			camera: this.camera,
+		});
+
+		window.requestAnimationFrame(this.update.bind(this));
+	}
+
+	/**
+	 * Listeners.
+	 */
+	addEventListeners() {
+		window.addEventListener("resize", this.onResize.bind(this));
+
+		window.addEventListener("mousewheel", this.onWheel.bind(this));
+		window.addEventListener("wheel", this.onWheel.bind(this));
+
+		window.addEventListener("mousedown", this.onTouchDown.bind(this));
+		window.addEventListener("mousemove", this.onTouchMove.bind(this));
+		window.addEventListener("mouseup", this.onTouchUp.bind(this));
+
+		window.addEventListener("touchstart", this.onTouchDown.bind(this));
+		window.addEventListener("touchmove", this.onTouchMove.bind(this));
+		window.addEventListener("touchend", this.onTouchUp.bind(this));
+	}
+}
+
+new App();
